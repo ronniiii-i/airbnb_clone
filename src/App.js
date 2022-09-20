d0573d55c46d05be45cadae85bec2b44e689eb56
Roni <ronniiii@yahoo.com>
2022-09-20 16:28:07 +0100

    props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import group from './images/group.png';
+// import group from './images/group.png';
 import './App.css';
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+
+import katie from './images/katie-zaferes.png';
+import wedding from './images/wedding.png';
+import bike from './images/bike.png';
+
 
 
 export default function App() {
@@ -11,6 +16,15 @@ export default function App() {
           <Navbar />
           <main>
             <Hero />
+            <Card 
+              img = {katie}
+              rating="5.0"
+              reviewcount={6}
+              country="USA"
+              title="Life Lessons with Katie"
+              price={136}
+              />
+            <Card />
             <Card />
           </main>
       </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,18 +2,18 @@ import React from "react"
 import katie from '../images/katie-zaferes.png';
 import star from '../images/star.png';
 
-export default function Card() {
-    return (
+export default function Card(props) {
+       return (
         <div className="card">
-            <img src={katie} className="card--image" />
+            <img src={props.img} className="card--image" />
             <div className="card--stats">
                 <img src={star} className="card--star" />
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewcount}) • </span>
+                <span className="gray">{props.country}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
